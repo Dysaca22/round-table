e209e48bb1b7c9a5e17f221f595ed70ab3941566
Dylan Cantillo <74154670+Dysaca22@users.noreply.github.com>
2025-08-25 18:31:48 -0500

    [ADD] gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/round-table/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      build: {
+        outDir: 'build'
       },
       resolve: {
         alias: {
